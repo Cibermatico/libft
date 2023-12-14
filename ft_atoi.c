12b635c31eb70a33cea240b6046ddc9fe7857c05
Cibermatico <149595043+Cibermatico@users.noreply.github.com>
2023-12-14 22:15:58 +0100

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (result * sign);
+}
+int main(void)
+{
+	char stringa[] = "---1w";
+	printf("%d ",ft_atoi(stringa));
+
+	return 0;
+}
