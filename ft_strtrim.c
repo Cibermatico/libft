9cc43f062dc5c54f831a9b829ad8959b95c865e3
Lorenzo Squarcia <lsquarci@e3r7p3.42roma.it>
2024-01-11 13:51:16 +0100

    aggiunti split, trim, substr, makefile da sistemare

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,23 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsquarci <lsquarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/09 19:29:38 by lsquarci          #+#    #+#             */
-/*   Updated: 2024/01/09 19:32:07 by lsquarci         ###   ########.fr       */
+/*   Created: 2024/01/11 13:37:16 by lsquarci          #+#    #+#             */
+/*   Updated: 2024/01/11 13:37:16 by lsquarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t i;
-	char *result;
+	int		start;
+	int		end;
+	char	*str;
 
-	i = 0;
-	result = (char *)malloc(sizeof(char) * s1);
-	while(s1)
-	{
-		/* da scrivere */
-	}
-	
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) && start <= end)
+		start++;
+	if (start > end)
+		return (ft_strdup(s1 + end + 1));
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	str = malloc(end - start + 2);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, &s1[start], end - start + 2);
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lsquarci <lsquarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 18:46:50 by lsquarci          #+#    #+#             */
-/*   Updated: 2024/01/10 18:30:17 by lsquarci         ###   ########.fr       */
+/*   Updated: 2024/01/11 13:47:42 by lsquarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,7 @@ int				ft_toupper(int c);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_calloc(size_t count, size_t size);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *s, char c);
 #endif
